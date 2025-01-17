26a977195ea7a6978e3739f5d0c5b9d8be2046ac
Julian Wilson <julianrwilson@gmail.com>
2025-01-17 19:05:40 +0800

    Adding minimum width

--- src/App.tsx
+++ src/App.tsx
@@ -37,11 +37,11 @@ const App = () => (
             <div className="flex-none">
               <AppSidebar />
             </div>
-            <main className="flex-1 w-full">
+            <main className="flex-1 w-full min-w-[800px]">
               <div className="p-4 border-b">
                 <SearchBar />
               </div>
-              <div className="p-6 container">
+              <div className="p-6 container min-w-[800px]">
                 <Routes>
                   <Route path="/" element={<Navigate to="/dashboard" replace />} />
                   <Route path="/dashboard" element={<Dashboard />} />
